fdafd3770a68fb62c23ad1d92e077b0e40955012
redig <luiz.speedsistemas@gmail.com>
2024-10-11 10:13:29 -0300

    feat(attributes): add charisma attribute and primary constructor to Attributes class

--- src/main/kotlin/dnk/library/attributes/Attributes.kt
+++ src/main/kotlin/dnk/library/attributes/Attributes.kt
@@ -1,11 +1,11 @@
 package dnk.library.attributes
 import java.io.Serializable
 
-class Attributes : Serializable {
-    var strength: Int = 8
-    var dexterity: Int = 8
-    var constitution: Int = 8
-    var intelligence: Int = 8
-    var wisdom: Int = 8
+class Attributes(
+    var strength: Int = 8,
+    var dexterity: Int = 8,
+    var constitution: Int = 8,
+    var intelligence: Int = 8,
+    var wisdom: Int = 8,
     var charisma: Int = 8
-}
+) : Serializable
